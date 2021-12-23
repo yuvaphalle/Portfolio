98a90b2869e27a1db8d326c4c443bdb4baa081b7
yuvaphalle <yuva.phalle@gmail.com>
2021-12-23 20:00:13 +0100

    Adding Auto Build

---
+++ build/precache-manifest.e6ccbc08f826ceb7751a208c0ddab553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce8383ae2351a13a7eb",
+    "url": "/static/css/main.fd6c2ab8.chunk.css"
+  },
+  {
+    "revision": "fce8383ae2351a13a7eb",
+    "url": "/static/js/main.fce8383a.chunk.js"
+  },
+  {
+    "revision": "2cbfea5fbe1262ba8a33",
+    "url": "/static/js/1.2cbfea5f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82750371a9c7765e4bf3e7e5f9f4a53b",
+    "url": "/index.html"
+  }
+];
